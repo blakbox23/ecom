daaf9d4902c08c749237a32dc0b0487249465f92
Peter MX <36844547+blakbox23@users.noreply.github.com>
2022-04-02 12:19:18 +0300

    Set up basic PDP page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Nav from './components/nav'
 import './App.css';
 import ProductsList from './components/ProductsList';
+import ProductDisplay from './components/ProductDisplay';
 
 function App() {
   return (
     <div className="App">
       <Nav />
-      <ProductsList />
+      {/* <ProductsList /> */}
+      <ProductDisplay />
     </div>
   );
 }
